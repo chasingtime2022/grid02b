e03e97d0ad821f9f04b2ee7fe303961e48ea19f1
Li Hongxin <105003661+chasingtime2022@users.noreply.github.com>
2023-04-05 23:54:30 +0800

    Add files via upload

---
+++ js/transit.js
@@ -0,0 +1,30 @@
+let dynamic = document.getElementById("dynamic");
+let current = document.getElementById("current");
+
+let n = 5;
+let m = n - 1;
+setInterval(() => {
+  dynamic.style.setProperty("z-index", n);
+  current.style.setProperty("z-index", m);
+  if (n > m) {
+    m = n;
+    n = n - 1;
+  } else {
+    n = m;
+    m = m - 1;
+  }
+}, 5000);
+
+// let n = "block";
+// let m = "none";
+// setInterval(() => {
+//   dynamic.setAttribute("display", n);
+//   current.setAttribute("display", m);
+//   if (n == "block") {
+//     n = "none";
+//     m = "block";
+//   } else {
+//     n = "block";
+//     m = "none";
+//   }
+// }, 100);
