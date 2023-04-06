ddc4a81499b0759d5b75ed376e977ccd52704399
Li Hongxin <105003661+chasingtime2022@users.noreply.github.com>
2023-04-06 10:40:51 +0800

    Update transit.js

--- js/transit.js
+++ js/transit.js
@@ -3,12 +3,12 @@ let current = document.getElementById("current");
 let energy = document.getElementById("energy");
 
 let n = 0;
-
+// let z
 setInterval(() => {
-  dynamic.style.setProperty("z-index", n / 3);
-  current.style.setProperty("z-index", (n + 1) / 3);
-  energy.style.setProperty("z-index", (n + 2) / 3);
-  if (n > 99) {
+  dynamic.style.setProperty("z-index", n % 3);
+  current.style.setProperty("z-index", (n + 1) % 3);
+  energy.style.setProperty("z-index", (n + 2) % 3);
+  if (n > 999) {
     n = 0;
   }
   n += 1;
