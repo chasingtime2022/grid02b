1f129708b39c6e973afe3e6cd8e30c5d3b0afe74
Li Hongxin <105003661+chasingtime2022@users.noreply.github.com>
2023-04-30 10:44:00 +0800

    Add files via upload

---
+++ js/news_update.js
@@ -0,0 +1,141 @@
+const news = [
+  {
+    title: "天格计划GRID",
+    img_url: "./news/grid_intro.png",
+    content:
+      "天格计划，全称“空间分布式伽马射线暴探测网”（Gamma Ray Integrated Detectors, GRID），是一个以学生为主体的、面向基础科学前沿的真正科学任务，同时也是一个理工学科交叉的基础科学人才培养项目。<br><br>天格计划旨在构建一个利用立方星搭载的、覆盖全天的伽马射线暴探测网络，首要观测目标是中子星并合产生的短伽马射线暴（引力波电磁对应体）样本。", // },
+  },
+  // {
+  //   title: "微沙龙 | 与“天格计划”面对面",
+  //   img_url: "./news/creators.jpg",
+  //   content:
+  //     "你是否听说过“追光少年”的故事，对“天格计划”充满好奇？你是否想知道一群本科生如何将探测器送上太空，去探测遥远宇宙的脉搏？<br><br>本次微沙龙，我们邀请到了“天格计划”团队成员与大家面对面交流，为大家讲述“天格计划”的故事，分享本科生科创经验。",
+  // },
+  {
+    title: "“天格计划”新年添新“芯”",
+    img_url: "./news/new_chip.jpg",
+    content:
+      "“天格计划”合作组成员、清华校友企业宇称电子发布了最新研发的多通道SiPM通用读出芯片ASIC-MPT2321，该型芯片在关键技术指标和功能接口方面针对“天格计划”的物理需求深度优化，将在后续的天格卫星载荷中进一步提升空间伽马探测器性能。",
+  },
+  {
+    title: "社团精品课-宇宙初啼",
+    img_url: "./news/lecture_gravity_wave.jpg",
+    content:
+      "4月6日晚，由中科院高能所粒子天体物理重点实验室的熊少林老师和刘聪展老师主讲的“宇宙初啼与宇宙烟花的探测”社团精品课顺利开展。<br><br>两位老师用最生动的语言向我们介绍了引力波、伽马射线暴等高能天体现象，为所有同学带来了一场别开生面的科普“大餐”。",
+  },
+  // {
+  //   title: "James Webb Space Telescope",
+  //   img_url: "./news/webb.png",
+  //   content:
+  //     "The James Webb Space Telescope is a giant leap forward in our quest to understand the Universe and our origins. Webb will examine every phase of cosmic history: from the first luminous glows after the Big Bang to the formation of galaxies, stars, and planets to the evolution of our own solar system.",
+  // },
+  {
+    title: "天格卫星载荷成功发射",
+    img_url: "./news/4grids_launch.gif",
+    content:
+      "春节前夕，1月15日11时14分，清华大学天格计划GRID-05B卫星载荷搭载于长光卫星MF02A07星 ，由长征二号丁运载火箭从太原卫星发射中心发射升空，成功进入预定轨道，卫星状态良好，发射获得圆满成功。",
+  },
+  {
+    title: "中科院高能所交流学习活动",
+    img_url: "./news/visit_ihep.jpg",
+    content:
+      "2022年10月8日，清华大学天格计划学生团队的同学们在中科院高能所粒子天体物理中心副主任熊少林老师（天格计划导师）的帮助下，组织了前往高能所的参观学习活动，近距离接触了粒子天体物理领域的前沿科学问题与大科学卫星、科学设施项目，粒子天体物理中心的专家老师们给天格的同学们带来了一场生动、全面的实践课。",
+  },
+];
+
+// const news = [
+//   {
+//     title: "天格计划GRID",
+//     img_url: "./news/grid_logo_1.jpg",
+//     content:
+//       "伽马暴的瞬时辐射机制有多种猜想，至今仍未有定论。2016年，天格学生团队对这一 前沿科学问题发起挑战。构建一个覆盖全天的伽马射线暴探测网络，监测、定位与引力波爆发成协的短伽马射线暴。",
+//   },
+//   {
+//     title: "天格计划创始成员",
+//     img_url: "./news/creators.jpg",
+//     content: "",
+//   },
+//   {
+//     title: "中子星并和",
+//     img_url: "./news/neutron.gif",
+//     content: "",
+//   },
+//   {
+//     title: "Hubble Telescope",
+//     img_url: "./news/hubble.png",
+//     content:
+//       "Named in honor of the trailblazing astronomer Edwin Hubble, the Hubble Space Telescope is a large, space-based observatory, which has revolutionized astronomy since its launch and deployment by the space shuttle Discovery in 1990. Far above rain clouds, light pollution, and atmospheric distortions, Hubble has a crystal-clear view of the universe. Scientists have used Hubble to observe some of the most distant stars and galaxies yet seen, as well as the planets in our solar system.",
+//   },
+//   {
+//     title: "James Webb Space Telescope",
+//     img_url: "./news/webb.png",
+//     content:
+//       "The James Webb Space Telescope is a giant leap forward in our quest to understand the Universe and our origins. Webb will examine every phase of cosmic history: from the first luminous glows after the Big Bang to the formation of galaxies, stars, and planets to the evolution of our own solar system.",
+//   },
+//   {
+//     title: "Perseverance Mars Rover",
+//     img_url: "./news/mars.jpg",
+//     content:
+//       'The Mars Perseverance rover mission is part of NASA\'s Mars Exploration Program, a long-term effort of robotic exploration of the Red Planet. The Mars Perseverance mission addresses high-priority science goals for Mars exploration, including key questions about the potential for life on Mars. The mission takes the next step by not only seeking signs of habitable conditions on Mars in the ancient past, but also searching for signs of past microbial life itself. The Mars Perseverance rover introduces a drill that can collect core samples of the most promising rocks and soils and set them aside in a "cache" on the surface of Mars.',
+//   },
+//   {
+//     title: "Parker Solar Probe",
+//     img_url: "./news/probe.png",
+//     content:
+//       "NASA's historic Parker Solar Probe mission is revolutionizing our understanding of the Sun, where changing conditions can propagate out into the solar system, affecting Earth and other worlds. Parker Solar Probe travels through the Sun’s atmosphere, closer to the surface than any spacecraft before it, facing brutal heat and radiation conditions to provide humanity with the closest-ever observations of a star.",
+//   },
+// ];
+
+let news_num = news.length;
+let count = 0;
+let n;
+setInterval(function () {
+  n = news_num - (count % news_num) - 1;
+  document.querySelector("#news_title").innerHTML = news[n].title;
+  document.querySelector("#news_figure").src = news[n].img_url;
+  document.querySelector("#news_content").innerHTML = news[n].content;
+  count += 1;
+}, 10000);
+
+// const news = [
+//   {
+//     title: "天格计划GRID",
+//     img_url: "./news/grid_logo_1.jpg",
+//     content:
+//       "伽马暴的瞬时辐射机制有多种猜想，至今仍未有定论。2016年，天格学生团队对这一 前沿科学问题发起挑战。构建一个覆盖全天的伽马射线暴探测网络，监测、定位与引力波爆发成协的短伽马射线暴。",
+//   },
+//   {
+//     title: "The First Members of GRID",
+//     img_url: "./news/creators.jpg",
+//     content: "",
+//   },
+//   {
+//     title: "Neutron Star Merger",
+//     img_url: "./news/neutron.gif",
+//     content: "",
+//   },
+//   {
+//     title: "Hubble Telescope",
+//     img_url: "./news/hubble.png",
+//     content:
+//       "Named in honor of the trailblazing astronomer Edwin Hubble, the Hubble Space Telescope is a large, space-based observatory, which has revolutionized astronomy since its launch and deployment by the space shuttle Discovery in 1990. Far above rain clouds, light pollution, and atmospheric distortions, Hubble has a crystal-clear view of the universe. Scientists have used Hubble to observe some of the most distant stars and galaxies yet seen, as well as the planets in our solar system.",
+//   },
+//   {
+//     title: "James Webb Space Telescope",
+//     img_url: "./news/webb.png",
+//     content:
+//       "The James Webb Space Telescope is a giant leap forward in our quest to understand the Universe and our origins. Webb will examine every phase of cosmic history: from the first luminous glows after the Big Bang to the formation of galaxies, stars, and planets to the evolution of our own solar system.",
+//   },
+//   {
+//     title: "Perseverance Mars Rover",
+//     img_url: "./news/mars.jpg",
+//     content:
+//       'The Mars Perseverance rover mission is part of NASA\'s Mars Exploration Program, a long-term effort of robotic exploration of the Red Planet. The Mars Perseverance mission addresses high-priority science goals for Mars exploration, including key questions about the potential for life on Mars. The mission takes the next step by not only seeking signs of habitable conditions on Mars in the ancient past, but also searching for signs of past microbial life itself. The Mars Perseverance rover introduces a drill that can collect core samples of the most promising rocks and soils and set them aside in a "cache" on the surface of Mars.',
+//   },
+//   {
+//     title: "Parker Solar Probe",
+//     img_url: "./news/probe.png",
+//     content:
+//       "NASA's historic Parker Solar Probe mission is revolutionizing our understanding of the Sun, where changing conditions can propagate out into the solar system, affecting Earth and other worlds. Parker Solar Probe travels through the Sun’s atmosphere, closer to the surface than any spacecraft before it, facing brutal heat and radiation conditions to provide humanity with the closest-ever observations of a star.",
+//   },
+// ];
